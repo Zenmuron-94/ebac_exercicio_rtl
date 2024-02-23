840b42a7bfced596c75c03726eb2a40934f907d0
Clóvis Pujol <bioclp@hotmail.com>
2024-02-23 08:13:26 -0300

    criando os arquivos de testes

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,30 @@
+
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
+describe('Teste para o comentario', () => {
+    test('Deve renderizar corretamente', () => {
+        render(<PostComment />);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
+    });
+
+    test('Deve adicionar dois comentários', () => {
+        render(<PostComment />);
+
+        fireEvent.change(screen.getByTestId('textarea-comentario'), {
+            target: {
+                value: 'Primeiro comentário adicionado',
+            }
+        });
+        fireEvent.click(screen.getByTestId('btn-comentar'));
+    
+        fireEvent.change(screen.getByTestId('textarea-comentario'), {
+            target: {
+                value: 'Segundo comentário adicionado',
+            }
+        });
+        fireEvent.click(screen.getByTestId('btn-comentar'));
+
+        expect(screen.getAllByTestId('comentario')).toHaveLength(2);
     });
 });
